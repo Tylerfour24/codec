0a8aa314633036a37e4c831649b2001f80102cdf
Tyler Gantt <tgantt@localhost.localdomain>
2015-11-18 10:49:45 -0500

    readbytes function successfully reads bytes and prints to screen

--- decoder.c
+++ decoder.c
@@ -7,31 +7,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+void readbytes(void);
+
+
+
+
+
 int main(void)
 {
-	printf("One.\n");
-	const char *directory = "/usr/local/share/codec/gps.pcap"; //read from this
-	const char *directory2 = strcat(getenv("HOME"), "work/.git/codec/sample2"); //write to this
-	printf("Two.\n");
+	readbytes();
+	return 0;
+}
 
-	int size = 1; 
-	
-	int descriptor = open(directory, O_RDONLY);
-	int descriptor2 = open(directory2, O_WRONLY);
-	printf("Three.\n");
+void readbytes(void) {
+	const char *directory = "/usr/local/share/codec/gps.pcap"; //read from this
+	char temp[100] = {'0'}; //store in this
+	//const char *directory2 = strcat(getenv("HOME"), "work/.git/codec/sample2.txt"); //write to this
+
+	unsigned int count;
 
 	FILE *input = fopen(directory, "r");
-	FILE *output = fopen(directory2, "w");
-	printf("Four.\n");
+	//FILE *output = fopen(directory2, "w");
 
-	read(descriptor, input, size);
-	write(descriptor2, output, size);
-	printf("Five.\n");
+	//read(descriptor, input, size);
+	//write(descriptor2, output, size);
 
 	fseek(input, 0, SEEK_SET); 
-	printf("Six.\n");
-	fread(&output, 1, 1, input); //i had input and output switched. oops.
-	printf("Seven.\n");
-	//this should read a byte from input and write it to output. it seems to be working. need to check, aka find out where output is.
-	return 0;
+	fread(temp, strlen(temp) +1, 1, input);
+
+	for(count = 0; count < strlen(temp); count++) {
+		printf("%x\n", temp[count]);
+	}
+
+	//successfully reading bytes and printing them to screen. 
 }
