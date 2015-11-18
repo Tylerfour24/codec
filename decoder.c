8ff1b65136da40a9bfe53ff9ad5f695ff1bbe19f
Tyler Gantt <tgantt@localhost.localdomain>
2015-11-18 15:45:06 -0500

    can read hello file completely and display all bits

--- decoder.c
+++ decoder.c
@@ -9,22 +9,33 @@
 
 void readbytes(void);
 
-
-
-
-
 int main(void)
 {
 	readbytes();
+	/*struct Headers {
+		char version[;
+		char sequence;
+		char type;
+		char length;
+		char source;
+		char dest;
+	} header;*/
+
+	int mask1 = 0xf0;
+	int mask2 = 0x0f;
+	int value = 0xff;
+	int result1 = mask1 & value;
+	int result2 = mask2 & value;
+	printf("%x\n", result1);
+	printf("%x\n", result2);
 	return 0;
 }
 
 void readbytes(void) {
-	const char *directory = "/usr/local/share/codec/gps.pcap"; //read from this
-	char temp[100] = {'0'}; //store in this
-	//const char *directory2 = strcat(getenv("HOME"), "work/.git/codec/sample2.txt"); //write to this
-
+	const char *directory = "/usr/local/share/codec/hello.pcap"; //read from this
+	unsigned char temp[200] = {0}; //store in this
 	unsigned int count;
+	//const char *directory2 = strcat(getenv("HOME"), "work/.git/codec/sample2.txt"); //write to this
 
 	FILE *input = fopen(directory, "r");
 	//FILE *output = fopen(directory2, "w");
@@ -33,9 +44,9 @@ void readbytes(void) {
 	//write(descriptor2, output, size);
 
 	fseek(input, 0, SEEK_SET); 
-	fread(temp, strlen(temp) +1, 1, input);
+	fread(temp, sizeof(temp), 1, input);
 
-	for(count = 0; count < strlen(temp); count++) {
+	for(count = 0; count < sizeof(temp); count++) {
 		printf("%x\n", temp[count]);
 	}
 
