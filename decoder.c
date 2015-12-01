b915ab1ba558a734b0564393cfdd5d948857fa57
Tyler Gantt <tgantt@localhost.localdomain>
2015-12-01 11:15:41 -0500

    Program can differentiate between the different pcap files, analyze them, and produce relevant readable format for them.

--- decoder.c
+++ decoder.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getfiletype(FILE *input, int *type);//create function to get source and destination IDS, or add said functionality into getfiletype function
+int getfiletype(FILE *input, int *type);
 void readfile(FILE *input, int *type);
 
 int main(void)
 {
-	const char *directory = "/usr/local/share/codec/hello.pcap"; //this will become argv[1] and subsequent argv[n]s may have to be added
+	const char *directory = "/usr/local/share/codec/status.pcap"; //this will become argv[1] and subsequent argv[n]s may have to be added
 	FILE *input = fopen(directory, "r");
 	int *type = malloc(sizeof(int*));
 
@@ -41,58 +41,142 @@ int getfiletype(FILE *input, int *type) {
 	printf("Sequence: %d\n", shiftbits); //prints sequence #
 
 	fseek(input, 86, SEEK_SET); //positioned to read source and destination IDs
-	fread(temp, 1, 8, input);
+	fread(temp, 4, 2, input);
 
-	printf("Source: %d.%d.%d.%d\n", temp[0], temp[1], temp[2], temp[3]);
-	printf("Destination: %d.%d.%d.%d\n", temp[4], temp[5], temp[6], temp[7]);
+	printf("Source: %d\n", temp[0]);
+	printf("Destination: %d\n", temp[1]);
 
 	return *type;
 }
 
-//this function determines packet length and prints the payload to screen (someday it'll even print it in a readable format)
+//this function determines packet length and prints the payload to screen
 void readfile(FILE *input, int *type) {
 
-	unsigned char temp[200] = {0}; //store in this first
-	unsigned char *next; //store in this second
+	unsigned char temp[200] = {0}; 
+	/*union Buffer {
+		unsigned char *cnext;
+		double *dnext;
+		float *fnext;
+	};
+	this union is meant to account for the different data types I have to deal with, but let's see if I can do it without first.
+	union Buffer buffer;*/
+		
+	unsigned char *next; //turn this into a union
 	int tempint;
-	int count;
 
-	fseek(input, 84, SEEK_SET); //positioned to read the length (2 bytes)
-	fread(temp, 2, 1, input);
+	//Consider making the following a switch statement
+	if(*type == 3) { //This section works
+		int count;
 
-	tempint = temp[1];
-	next = calloc(tempint, 1);
+		printf("MESSAGE\n");
+		fseek(input, 84, SEEK_SET); //positioned to read the length (2 bytes)
+		fread(temp, 1, 2, input);
 
-	fseek(input, 94, SEEK_SET);
-	fread(next, 1, tempint, input);
+		tempint = temp[1];
+		next = calloc(tempint, 1);
 
+		fseek(input, 94, SEEK_SET);
+		fread(next, 1, tempint, input);
 
-	if(*type == 3) {
-		printf("MESSAGE: ");
 		for(count = 0; count < tempint; count++) {
 			printf("%c", next[count]);
 		}
+		free(next);
 	}
 
-	else if (*type == 2) {
-		printf("GPS:\n");
-		for(count = 0; count < tempint; count++) {
-			printf("%x", next[count]); //prints hex values until I figure out how to make it human readable
-		}
+	else if (*type == 2) { //This section works (tentatively, values could be wrong but it produces values)
+		float longitude;
+		float latitude;
+		float altitude;
+
+		printf("GPS\n");
+		next = calloc(2, 8);
+		fread(next, 8, 2, input);
+
+		longitude = next[0];
+		latitude = next[1];
+		
+		printf("Longitude: %.2f\n", longitude);
+		printf("Latitude: %.2f\n", latitude);
+		free(next);
+
+		next = calloc(5, 4);
+		fread(next, 4, 5, input);
+
+		altitude = next[4];
+
+		printf("Altitude: %.2f", altitude);
+		free(next);
 	}
 
 	else if (*type == 1) {
-		printf("COMMAND:\n");
-		for(count = 0; count < tempint; count++) {
-			printf("%x", next[count]); //prints hex values until I figure out how to make it human readable
+		unsigned short command;
+		unsigned short parameter;
+
+		printf("COMMAND\n");
+		next = calloc(2, 2);
+		fread(next, 2, 2, input);
+
+		command = next[0];
+		parameter = next[1];
+
+		printf("Command: %d\n", command);
+		switch(command) {
+			case 0:
+				printf("GET_STATUS");
+				break;
+			case 1:
+				printf("SET_GLUCOSE");
+				break;
+			case 2:
+				printf("GET_GPS");
+				break;
+			case 3:
+				printf("SET_CAPSAICIN");
+				break;
+			case 4:
+				printf("GET_STATUS");
+				break;
+			case 5:
+				printf("SET_OMORFINE");
+				break;
+			case 6:
+				printf("GET_STATUS");
+				break;
+			case 7:
+				printf("REPEAT");
+				break;
 		}
+		if( (command % 2) != 0) {
+			printf("\nParameter: %d", parameter);
+		}
+		free(next);
 	}
 
-	else if (*type == 0) {
-		printf("STATUS:\n");
-		for(count = 0; count < tempint; count++) {
-			printf("%x", next[count]); //prints hex values until I figure out how to make it human readable
-		}
+	else if (*type == 0) { //This section works (tentatively, values could be wrong but it produces values)
+		double battery;
+		unsigned short glucose;
+		unsigned short capsaicin;
+		unsigned short omorfine;
+
+		printf("STATUS\n");
+		next = calloc(1, 8);
+		fread(next, 8, 1, input);
+
+		battery = next[0];
+		printf("Battery Power: %f\n", battery);
+		free(next);
+
+		next = calloc(7, 2);
+		fread(next, 2, 7, input);
+		
+		glucose = next[4];
+		capsaicin = next[5];
+		omorfine = next[6];
+		printf("Glucose: %d\n", glucose);
+		printf("Capsaicin: %d\n", capsaicin);
+		printf("Omorfine: %d", omorfine);
+		free(next);
 	}
 
 	else {//this successfully differentiates between the different "type" values
@@ -100,7 +184,6 @@ void readfile(FILE *input, int *type) {
 	}
 
 	printf("\n");
-	free(next);
 }
 
 /*note byte format of .pcap files is as follows:
